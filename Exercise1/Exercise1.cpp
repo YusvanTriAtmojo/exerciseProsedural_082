997f9d3116209cfd20e798f2628d0adb24e4711c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-29 10:38:31 +0700

    Melakukan proses utama dengan tipe data int

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -11,4 +11,29 @@ string status(double s, double m) {
 		return "gagal";
 }
 
+int main() {
+	double nilM, nilBI;
+	string Nama;
+	int i;
+	string n[20];
+	cout << "Memasukkan Data" << endl;
+	for (int i = 0; i < 20; i++) {
+		cout << "No ke -" << (i + 1) << endl;
+		cout << "Masukan nama : " << "\n" << (i + 1) << ": ";
+		cin >> n[i];
+		cout << "Masukan nilai Matematika : ";
+		cin >> nilM;
+		cout << "Masukan nilai bahasa inggris : ";
+		cin >> nilBI;
+	}
+	cout << "\n\n=================" << endl;
+	cout << "Laporan kelulusan" << endl;
+	cout << "=================" << endl;
 
+	cout << "\nNo\tNama\tStatus\t" << endl;
+	cout << "====================" << endl;
+	for (i = 1; i < 21; i++) {
+		cout << i << "\t" << n[i] << "\t" << status(rerata(nilM, nilBI), nilM) << endl;
+	}
+	return 0;
+}
