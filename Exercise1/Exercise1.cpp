d8803b262a989f9a26ba700fbf860041f0d6e926
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-29 10:38:05 +0700

    menambahkan prosedur string yaitu status

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -4,3 +4,11 @@ using namespace std;
 double rerata(double a, double b) {
 	return (a + b) / 2;
 }
+string status(double s, double m) {
+	if (s >= 70 || m > 80)
+		return "lulus";
+	else
+		return "gagal";
+}
+
+
